4023dff2bb467c53e6fcdaceb3c0058b0261facc
Joshua04d <joshua04dcosta@gmail.com>
2026-02-28 03:23:18 +0530

    "2: Configure PWA with next-pwa and manifest"

---
+++ fitness-dashboard/next.config.js
@@ -0,0 +1,10 @@
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports = withPWA({
+  reactStrictMode: true,
+  turbopack: {}, // 👈 This silences the Turbopack conflict
+});
